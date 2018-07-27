cafe52ae402cf9aa0c3cc33a0f7d4f607503c643
Will Leinweber <will@bitfission.com>
2018-07-26 23:52:40 -0700

    show distance

--- src/print_emoji.cr
+++ src/print_emoji.cr
@@ -1,10 +1,12 @@
 require "./ppm2emoji/*"
 
+zero = Pixel.new(0_u8, 0_u8, 0_u8)
+
 i = (ARGV[0]? || "5000").to_i
 Emoji.load do |e|
   e.q2.to_c; e.q1.to_c; puts " #{e.text}"
   e.q3.to_c; e.q4.to_c
-  puts " #{e.emoji}"
+  puts " #{e.emoji} #{zero - e.q1} #{e.q1 - zero}"
   i -= 1
   exit if i < 0
 end
